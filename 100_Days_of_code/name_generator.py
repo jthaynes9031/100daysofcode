af268493eaa7f933d536c2f9708dc7efe533dc94
Jonathan Haynes <55987768+jthaynes9031@users.noreply.github.com>
2021-11-17 10:33:24 -0500

    Update name_generator.py

--- 100_Days_of_code/name_generator.py
+++ 100_Days_of_code/name_generator.py
@@ -1,4 +1,6 @@
 print("Welcome to band name Generator")
 city = input("what city are you from?\n")
 food = input("Name of your favorite Food?\n")
-print("Name of your Band is " + city + " " + food)
+#print("Name of your Band is " + city + " " + food)
+bandname = "Name of you band can be {0} {1} or {1} {0}".format(city,food)
+print(bandname)
