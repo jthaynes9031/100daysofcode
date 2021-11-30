5d0fbf6b3067c1e20fc7a9bc038d9cff7d7ef38f
Jonathan Haynes <55987768+jthaynes9031@users.noreply.github.com>
2021-11-29 21:26:02 -0500

    Add files via upload

--- 100_Days_of_code/Coding_cypher.py
+++ 100_Days_of_code/Coding_cypher.py
@@ -1,18 +1,26 @@
 import sys
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+yes_no = True
+while yes_no:
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' , 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))  
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    def caesar(start_text, shift_amount, cipher_direction):
+        end_text = ""
+        if cipher_direction == "decode":
+            shift_amount *= -1
+        for letter in start_text:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        print(f"Here's the {direction}d result: {end_text}")
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
-encrypt(plain_text= text, shift_amount= shift)
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    result = input("do you want to continue type 'yes' if not type 'no'")
+    if result == 'no':
+        yes_no = False
+    else:
+        yes_no =True
     
